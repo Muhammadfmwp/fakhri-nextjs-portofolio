268010d63cbe1a9d683a8b3bebf6261d81b72978
muhammad.fakhri <muhammad.fakhri@admo.astra.co.id>
2024-03-12 10:21:03 +0700

    fix ' to &apos

--- components/Header.tsx
+++ components/Header.tsx
@@ -15,7 +15,7 @@ const Header = () => {
           >
             <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
           </svg>
-          <span className="ml-3 text-xl">Fakhri's Portofolio </span>
+          <span className="ml-3 text-xl">Fakhri&apos;s Portofolio </span>
         </a>
         <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
           <a className="mr-5 hover:text-gray-900">First Link</a>
